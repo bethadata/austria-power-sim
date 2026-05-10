fe10549f387f1c557a8c012d225b9a7e4aa60707
bernhardthaler <bernhard.thaler@gmx.net>
2026-05-10 22:36:22 +0200

    margins fine tuning

--- src/components/sections/IntroCard.vue
+++ src/components/sections/IntroCard.vue
@@ -1,12 +1,15 @@
 <template>
   <v-card class="ps-2 mb-0 elevation-2">
-    <v-row>
-      <v-col>
-        <v-card-title class="text-h5 font-weight-bold" style="white-space: normal">
+    <v-row class="mb-0 pb-0">
+      <v-col class="pb-0">
+          <v-card-title class="text-h5 font-weight-bold" style="white-space: normal">
           {{ t('intro_card.welcome_text') }}
         </v-card-title>
-
-        <v-card-text class="text-body-medium">
+      </v-col>
+    </v-row>
+    <v-row class="mt-0 pt-0">
+      <v-col cols="12" md="6" class="pt-1 pb-1">
+        <v-card-text class="text-body-medium pt-0 pb-1">
           <i18n-t keypath="intro_card.intro_text" tag="span" scope="global">
           <template #intro_text_bold>
             <strong> {{ t('intro_card.intro_text_bold') }}  </strong>
@@ -14,9 +17,9 @@
           </i18n-t>
         </v-card-text>
 
-          </v-col>
-       <v-col>
-          <v-alert type="info" variant="tonal" class="my-alert ma-6 mt-8 mb-0">
+       </v-col>
+       <v-col cols="12" md="6" class = "pb-1">
+          <v-alert type="info" variant="tonal" class="my-alert ma-6 ml-3 mt-1 mb-2">
           <i18n-t keypath="intro_card.info_box_text" tag="span" scope="global">
           <template #info_box_link>
             <router-link to="/model" class="text-primary font-weight-medium">
